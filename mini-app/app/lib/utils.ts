a9896ae57fb22bba75fcef5d0f8ae3614b3c50b3
dylsteck <dylancsteck@gmail.com>
2024-11-22 00:11:54 -0500

    wip: copy `recbot` to new `mini-app` repo and remove all unused code for `mini-app`

---
+++ mini-app/app/lib/utils.ts
@@ -0,0 +1,36 @@
+import { NeynarAPIClient } from '@neynar/nodejs-sdk';
+import { User } from '@neynar/nodejs-sdk/build/neynar-api/v2';
+import { IcebreakerProfile } from './types';
+
+export const ICEBREAKER_API_URL = 'https://app.icebreaker.xyz/api';
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
+
+export const ICEBREAKER_CREDENTIALS_URL =
+  'https://app.icebreaker.xyz/credentials';
+export const neynarClient = new NeynarAPIClient(
+  process.env.NEYNAR_API_KEY ?? ''
+);
+
+type ProfileResponse = {
+  profiles: IcebreakerProfile[];
+};
+
+export const getIcebreakerProfileFromFname = async (
+  fname?: string
+): Promise<IcebreakerProfile | undefined> => {
+  if (!fname) {
+    return;
+  }
+  try {
+    const response = await fetch(`${BASE_URL}/api/fname?fname=${fname}`);
+    if (!response.ok) {
+      throw new Error('Error fetching data for fname');
+    }
+    const json: ProfileResponse = await response.json();
+    return json.profiles[0];
+  } catch (err) {
+    console.error(err);
+    return;
+  }
+};
---
+++ mini-app/app/lib/webhook.ts
@@ -0,0 +1,121 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { createHmac } from 'crypto';
+import { type NextRequest } from 'next/server';
+import {
+  type IcebreakerStoreCredentialsParams,
+  type WebhookData,
+} from './types';
+import {
+  getEthAddressForUser,
+  ICEBREAKER_API_URL,
+  ICEBREAKER_CREDENTIALS_URL,
+  neynarClient,
+} from './utils';
+import { attestationsSchemas, isValidRec } from './attestation-matcher';
+
+export async function extractEndorsementFromCast(webhook: WebhookData) {
+  const recResp = await isValidRec(
+    webhook.data.text,
+    webhook.data.mentioned_profiles,
+    webhook.data.author.username
+  );
+  if (recResp.isValid) {
+    const attesterAddress = await getEthAddressForUser(webhook.data.author);
+    if(attesterAddress === '0x'){
+      throw new Error('Attester address not found');
+    }
+    const attesteeUser = webhook.data.mentioned_profiles.find(
+      (profile) => profile.username === recResp.mentionedUsername
+    );
+    const attesteeAddress = attesteeUser
+      ? await getEthAddressForUser(attesteeUser)
+      : '0x';
+    const schema = attestationsSchemas.find(
+      (schema) => schema.name === recResp.schemaName
+    );
+    const json: IcebreakerStoreCredentialsParams = {
+      attesterAddress: attesterAddress,
+      attesteeAddress: attesteeAddress,
+      isPublic: true,
+      name: recResp.schemaName,
+      schemaID: schema?.schemaID ?? '-1',
+      source: 'Farcaster',
+      reference: webhook.data.hash,
+      timestamp: webhook.data.timestamp,
+      uid: `${webhook.data.hash}000000000000000000000000`,
+    };
+    try {
+      const response = await fetch(`${ICEBREAKER_API_URL}/v1/credentials`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${process.env.ICEBREAKER_BEARER_TOKEN}`,
+        },
+        body: JSON.stringify(json),
+      });
+
+      const encodedCredentialName = encodeURIComponent(recResp.schemaName);
+
+      // TODO: parse `response.json` and check the message field instead of just checking for `response.ok`
+      await neynarClient.publishCast(
+        process.env.NEYNAR_SIGNER_UUID ?? '',
+        response.ok
+          ? `Success! Visit ${ICEBREAKER_CREDENTIALS_URL}/${encodedCredentialName}?show=givers&receivers=${attesteeAddress} to view on Icebreaker.`
+          : 'Beep boop. Something went wrong.',
+        {
+          replyTo: webhook.data.hash,
+        }
+      );
+    } catch (err) {
+      return (err as Error).message;
+    }
+  } else {
+    try {
+      await neynarClient.publishCast(
+        process.env.NEYNAR_SIGNER_UUID ?? '',
+        recResp.schemaName
+          ? `You must receive an endorsement for ${recResp.schemaName} before you can endorse others.`
+          : 'Unable to endorse. Make sure to format with: (at)rec (at)<username> <endorsement>',
+        {
+          replyTo: webhook.data.hash,
+        }
+      );
+    } catch (err) {
+      return (err as Error).message;
+    }
+  }
+}
+
+export async function processWebhookBody(webhook: WebhookData) {
+  if (webhook.type !== 'cast.created' || !webhook.data) {
+    throw new Error('Invalid webhook payload');
+  }
+
+  await extractEndorsementFromCast(webhook);
+  return { success: true };
+}
+
+export async function verifyWebhookSignature(req: NextRequest): Promise<any> {
+  const body = await req.text();
+
+  const sig = req.headers.get('X-Neynar-Signature');
+  if (!sig) {
+    throw new Error('X-Neynar-Signature is missing from the request headers');
+  }
+
+  const webhookSecret = process.env.NEYNAR_WEBHOOK_SECRET;
+  if (!webhookSecret) {
+    throw new Error('Make sure to set NEYNAR_WEBHOOK_SECRET in your .env file');
+  }
+
+  const hmac = createHmac('sha512', webhookSecret);
+  hmac.update(body);
+
+  const generatedSignature = hmac.digest('hex');
+
+  const isValid = generatedSignature === sig;
+  if (!isValid) {
+    throw new Error('Invalid Neynar webhook signature');
+  }
+  return body;
+}
